144e309f37874b11430883612d189b69c4e51068
victoria633 <victoria6411@hotmail.com>
2018-06-05 05:39:19 +0000

    Done.

---
+++ about_me.rb
@@ -0,0 +1,20 @@
+def about_me
+  puts "my name is victoria"
+  puts"I grew up in pheonix"
+  puts"I'm 17 years old"
+  puts"my favorite food is ice cream"
+end
+
+30.times do
+about_me
+end
+
+puts"how are you feeling?"
+answer = gets.chomp
+puts "so you'er feeling #{answer}?"
+
+puts"how old are you?"
+answer =gets.chomp
+puts"so you'er #{answer}?"
+
+#puts "how "
